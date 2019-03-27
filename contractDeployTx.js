a7e890ec51db990e9d7bd7b6b574e3f530abcc48
yanesh <yaneshcode@gmail.com>
2019-03-27 19:44:22 -0400

    raw tx

--- contractDeployTx.js
+++ contractDeployTx.js
@@ -2,26 +2,18 @@ const ethers = require('ethers');
 const credentials = require('./credentials/credentials.js');
 const build = require('./build/contracts/Ballot.json');
 
-// Connect to the network
 let provider = new ethers.providers.InfuraProvider("kovan", credentials.INFURA_API_KEY);
 
-// Load the wallet to deploy the contract with
 let wallet = new ethers.Wallet(credentials.P_KEY, provider);
 
-
-// The Contract interface
 let abi = build.abi;
 
-// The bytecode from Solidity, compiling the above source
 let bytecode = build.bytecode;
 
-// Deployment is asynchronous, so we use an async IIFE
 (async function() {
 
   //Data = bytecode+params
-
   let params = '0000000000000000000000000000000000000000000000000000000000000001';
-
   let data = bytecode + params;
 
   const gasPrice = ethers.utils.bigNumberify('1000000000');
@@ -48,7 +40,7 @@ let bytecode = build.bytecode;
     // let factory = new ethers.ContractFactory(abi, bytecode, wallet);
     // console.log(factory);
 
-    // const unsignedTx = factory.getDeployed
+    // const unsignedTx = factory.getDeployed(1, {gasLimit:, gasPrice: })
     //
     // // Notice we pass in "Hello World" as the parameter to the constructor
     // let contract = await factory.deploy("1");
